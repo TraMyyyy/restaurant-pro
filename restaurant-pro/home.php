226b5795ed495ce53ea8cbce2dcfdfb30198ca34
TraMyyyy <ttmy2061@gmail.com>
2021-09-16 23:11:00 +0700

    restaurant project

---
+++ restaurant-pro/home.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- Logo and menu start here -->
+    <section class = "navbar">
+        <div class = "container ">
+        <div class = "logo">
+            <img src="images/logo.png" alt="Restaurant Logo" class = "img-responsive">
+        </div>
+
+        <div class = "menu text-right">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+
+                <li>
+                    <a href="#">Categories</a>
+                </li>
+
+                <li>
+                    <a href="#">Foods</a>
+                </li>
+
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class = "clearfix"></div>
+        </div>
+       
+    </section>
+    <!-- Logo and menu end here -->
+
+    <!-- Search start here -->
+    <section class = "food-search">
+        <div class ="container">
+           <form action="">
+               <input type="search" name = "search" placeholder = "Search for Food...">
+               <input type="submit" name = "submit" value = "Search">
+           </form>
+        </div>
+    </section>
+    <!-- Search end here -->
+
+    <!-- Explore food  start here -->
+    <section class = "ex-food">
+        <div class = "container ">
+        <div class = "explore-food">
+            <center><strong>Explore Foods</strong></center>
+
+        </div>
+
+        <div class = "ex-box">
+            Box 1
+
+        </div>
+
+        <div class = "ex-box">
+            Box 2
+
+        </div>
+
+        <div class = "ex-box">
+            Box 3
+
+        </div>
+        </div>
+    </section>
+    <!-- Explore food end here -->
+
+    <!-- Food menu start here -->
+    <section class = "food-menu">
+         <div class = "container ">
+            <div class = "food-me">
+                <strong><center>Food Menu</center></strong>
+            </div>
+
+            <div class = "me-box">
+                Box 1
+
+            </div>
+
+            <div class = "me-box">
+                Box 2
+
+            </div>
+
+            <div class = "me-box">
+                Box 3
+
+            </div>
+
+            <div class = "me-box">
+                Box 4
+
+            </div>
+
+            <div class = "me-box">
+                Box 5
+
+            </div>
+
+            <div class = "me-box">
+                Box 6
+
+            </div>
+
+            <div class = "me-footer">
+                see all foods here
+            </div>
+            </div>
+
+    </section>
+    <!-- Food menu end here -->
+
+    <!-- Social media start here -->
+    <section class = "social">
+        <div class = "container ">
+        <ul>
+           <li>
+               <a href="#" >Facebook</a>
+            </li>
+        <li>
+               <a href="#" >Instagram</a>
+            </li>
+        <li>
+               <a href="#" >Twitter</a>
+            </li>
+            
+        </ul>
+
+        
+        </div>
+    </section>
+    <!-- Social media end here -->
+
+    <!-- Footer start here -->
+    <section class = "footer">
+        <div class = "container ">
+        <center><p>All rights reserved. Designed By <a href="#">CSE.TLU</a></p></center>
+         </div>
+    </section>
+    <!-- Footer end here -->
+</body>
+</html>
